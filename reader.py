bedabb9cf6a7b4189b7a0c66c99e2c60e8498e52
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2016-12-30 20:17:30 +0100

    Wrap reader functions into class

--- reader.py
+++ reader.py
@@ -1,31 +1,33 @@
 import numpy as np
 
-LABEL_SIZE = 1
+class Reader:
+	label_size = 1
+	file_name = 'patterns'
 
-def read_patterns():
-	file = open('patterns', 'r')
-	read_lines = file.read().splitlines()
-	lines = list(filter(None, read_lines))
-	pattern_size = int(lines[0])
-	patterns = _parse_patterns(pattern_size, lines[1:])
-	return {'pattern_size': pattern_size, 'patterns': patterns}
+	def patterns(self):
+		file = open(self.file_name, 'r')
+		read_lines = file.read().splitlines()
+		lines = list(filter(None, read_lines))
+		self.pattern_size = int(lines[0])
+		patterns = self._parse_patterns(lines[1:])
+		return {'pattern_size': self.pattern_size, 'patterns': patterns}
 
-def _parse_patterns(pattern_size, patterns_lines):
-	patterns = {}
-	for i in range(0, len(patterns_lines), pattern_size + LABEL_SIZE):
-		pattern_lines = patterns_lines[i:i + pattern_size + LABEL_SIZE]
-		pattern = _parse_pattern(pattern_size, pattern_lines)
-		patterns.update(pattern)
-	return patterns
+	def _parse_patterns(self, patterns_lines):
+		patterns = {}
+		for i in range(0, len(patterns_lines), self.pattern_size + self.label_size):
+			pattern_lines = patterns_lines[i:i + self.pattern_size + self.label_size]
+			pattern = self._parse_pattern(pattern_lines)
+			patterns.update(pattern)
+		return patterns
 
-def _parse_pattern(pattern_size, pattern_lines):
-	label = pattern_lines[0]
-	pattern = pattern_lines[1:pattern_size + LABEL_SIZE]
-	return { label: _to_sgn(pattern_size, pattern) }
+	def _parse_pattern(self, pattern_lines):
+		label = pattern_lines[0]
+		pattern = pattern_lines[1:self.pattern_size + self.label_size]
+		return { label: self._to_sgn(pattern) }
 
-def _to_sgn(pattern_size, pattern_lines):
-	result = np.full((pattern_size, pattern_size), -1)
-	for row_idx, row in enumerate(pattern_lines):
-		for col_idx, char in enumerate(row):
-			if char != ' ': result[row_idx][col_idx] = 1
-	return result
+	def _to_sgn(self, pattern_lines):
+		result = np.full((self.pattern_size, self.pattern_size), -1)
+		for row_idx, row in enumerate(pattern_lines):
+			for col_idx, char in enumerate(row):
+				if char != ' ': result[row_idx][col_idx] = 1
+		return result
