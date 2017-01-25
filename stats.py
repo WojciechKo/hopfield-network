621667f21e3a922def7e241ce2f5289923afab56
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2017-01-26 00:52:00 +0100

    Wrap recall into method

--- stats.py
+++ stats.py
@@ -11,6 +11,18 @@ def generate_patterns(patterns_count, size):
     generate_pattern = lambda i, s: ("pattern-" + str(i), generate_pattern_array(s))
     return dict([generate_pattern(i, s) for i, s in enumerate([size] * patterns_count)])
 
+
+def try_to_recall(pattern_to_recall, tries):
+    result = {}
+    pattern = pattern_to_recall
+    for i in range(tries):
+        result = hopfield.recall(pattern)
+        pattern = result["pattern"]
+        print(i)
+        if result["stable"] or result["name"]: return result
+
+    return result
+
 patterns_count = 10
 pattern_size = 10
 
@@ -20,12 +32,7 @@ init_pattern = generate_pattern_array(pattern_size)
 
 new_pattern = init_pattern
 
-for i in range(10):
-    result = hopfield.recall(new_pattern)
-    new_pattern = result["pattern"]
-    stable = result["stable"]
-    name = result["name"]
+result = try_to_recall(init_pattern, 10)
 
-    print(new_pattern)
-    print(stable)
-    print(name)
+print(result["stable"])
+print(result["name"])
