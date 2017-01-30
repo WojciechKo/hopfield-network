189b8aec301a3ec4096c4f9c970676b62c8a3848
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2017-01-30 23:46:31 +0100

    Working stas script

--- stats.py
+++ stats.py
@@ -11,28 +11,46 @@ def generate_patterns(patterns_count, size):
     generate_pattern = lambda i, s: ("pattern-" + str(i), generate_pattern_array(s))
     return dict([generate_pattern(i, s) for i, s in enumerate([size] * patterns_count)])
 
-
-def try_to_recall(pattern_to_recall, tries):
+def try_to_recall(hopfield, pattern_to_recall, tries = 20):
     result = {}
     pattern = pattern_to_recall
     for i in range(tries):
         result = hopfield.recall(pattern)
         pattern = result["pattern"]
-        print(i)
         if result["stable"] or result["name"]: return result
 
     return result
 
-patterns_count = 10
-pattern_size = 10
+def corrupt_pattern(name, original_pattern, percentage):
+    pattern = np.copy(original_pattern)
+    count = int(pattern.size * percentage)
+    chosen = np.random.choice(range(pattern.size), count, replace=False)
+    for index in chosen:
+        pattern[index] *= -1
+    return (name, pattern)
 
-patterns = generate_patterns(patterns_count, pattern_size)
-hopfield = Hopfield(patterns)
-init_pattern = generate_pattern_array(pattern_size)
+def test_pattern(hopfield, name, pattern):
+    result = try_to_recall(hopfield, pattern)
+    return 1 if result["name"] == name else 0
 
-new_pattern = init_pattern
+def test_hopfield(hopfield):
+    corruptions = np.arange(0, 0.5, 0.05)
 
-result = try_to_recall(init_pattern, 10)
+    for corruption in corruptions:
+        result = [test_pattern(hopfield, *corrupt_pattern(*pattern, corruption)) for pattern in hopfield.patterns.items()]
+        print("Corruption: " + '{:.1%}'.format(corruption), end="\t")
+        print("Result: " + '{:.1%}'.format(np.average(result)))
 
-print(result["stable"])
-print(result["name"])
+network_size = 900
+pattern_size = int(np.sqrt(network_size))
+
+network_fullness = np.arange(0.05, 0.15, 0.01)
+for fullness in network_fullness:
+    print("Fullness: " + str(fullness))
+
+    patterns_count = int(network_size * fullness)
+    patterns = generate_patterns(patterns_count, pattern_size)
+    hopfield = Hopfield(patterns)
+
+    test_hopfield(hopfield)
+    print("\n")
